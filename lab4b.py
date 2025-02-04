53d46e0e7f15bac814887b299451dd4fc2c35285
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-04 14:22:19 -0600

    Upside down triangle first pass with subtraction

---
+++ lab4b.py
@@ -0,0 +1,6 @@
+#Set rows and loop, printing a * for the row number minus the outer loops current iteration
+rows = 7
+for r in range(rows):
+    for s in range(rows - r):
+        print('*', end='')
+    print()
