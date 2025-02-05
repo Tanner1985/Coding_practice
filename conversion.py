670625a21c08b5d2bbdd98d93a4c08a2aae592d2
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-05 11:25:10 -0600

    Moving conversations to external module for lab 5B

---
+++ conversion.py
@@ -0,0 +1,16 @@
+#Functions to convert a given value to metric and return the converted number
+def InchesToCm(inches_converter):
+        inchesToCm = inches_converter * 2.54
+        return inchesToCm
+def PoundsToKg(pounds_converter):
+        poundsToKl = pounds_converter * .45
+        return poundsToKl
+def GalToLit(gallons_converter):
+        gallonsToL = gallons_converter * 3.9
+        return gallonsToL
+def FahToCel(fahren_converter):
+        fahrenheitToC = (fahren_converter - 32) * 5/9
+        return fahrenheitToC
+def milesToKm(miles_converter):
+        milToKm = miles_converter * 1.6
+        return milToKm
--- lab5Ba.py
+++ lab5Ba.py
@@ -1,5 +1,6 @@
 #Define Main function, get input and check for validity, then give to function to calculate and return converstion
 def main():
+   import conversion
    bad_counter = 0
    miles_converter = float(input('William, please input the miles you wish to convert to kilometers'))
    while(miles_converter < 0 and bad_counter < 2):
@@ -51,25 +52,10 @@ def main():
                     else:
                         InchesToCm(inches_converter)
 
-#Function to convert miles to kilometers
-def milesToKm(miles_converter):
-        milToKm = miles_converter * 1.6
         print (f'William, {miles_converter} miles is {milToKm:.2f} kilometers. Quite a long shot huh?')
-#Function to  convert Fahrenheit to Celsius
-def FahToCel(fahren_converter):
-        fahrenheitToC = (fahren_converter - 32) * 5/9
         print (f'William, {fahren_converter} degrees fahrenheit is {fahrenheitToC:.2f} degrees celsius. That is so cool!')
-#Function to convert Gallons to Liters    
-def GalToLit(gallons_converter):
-        gallonsToL = gallons_converter * 3.9
         print (f'William, {gallons_converter} gallons comes out to {gallonsToL:.2f} liters. Should I turn down the volume?')
-#Function to convert Pounds to Kilograms
-def PoundsToKg(pounds_converter):
-        poundsToKl = pounds_converter * .45
         print (f'William, {pounds_converter} pounds turns into {poundsToKl:.2f} kilograms. That was some heavy math!')
-#Function to convert Inches to Centimeters    
-def InchesToCm(inches_converter):
-        inchesToCm = inches_converter * 2.54
         print (f'William, {inches_converter} inches is {inchesToCm:.2f} centimeters. I have no more puns, but that was fun!')
 
 main()
