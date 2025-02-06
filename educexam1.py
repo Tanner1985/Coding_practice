88f68087ba7090de4422968d630bb8a55b7cf8fe
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-06 08:28:31 -0600

    First pass at study questions for Educ 1st exam

--- educexam1.py
+++ educexam1.py
@@ -3,7 +3,7 @@ def main():
     questionNum = random_number()
     question_list = questionList()
     answer_list = answerList()
-    answer = input(f'{question_list[questionNum]}')
+    answer = input(f'{question_list[questionNum]}: ')
     print(f'You answered: {answer}')
     print(f'The correct answer is: {answer_list[questionNum]}')
     play_again = input('Would you like to play again? (yes or no): ')
@@ -18,8 +18,10 @@ def random_number():
     return random_number
 
 def questionList():
-    question_list = ['What is the capital of France?', 'What is the capital of Germany?', 'What is the capital of Italy?', 'What is the capital of Spain?', 'What is the capital of the United States?', 'What is the capital of Canada?', 'What is the capital of Mexico?', 'What is the capital of Brazil?', 'What is the capital of Argentina?', 'What is the capital of Australia?']
+    question_list = ['What are the four external factors in the model of strategic learning?', 'What are the three internal factors of strategic learning?', 'Give two examples of each internal factor of the stratigic learning model', 'List your top two intelligences from the multiple intelligence list', 'What are two study skills for each of your intelligences?', 'What does SMART mean when making goals?', 'What is the capital of Mexico?', 'What is the capital of Brazil?', 'What is the capital of Argentina?', 'What is the capital of Australia?']
     return question_list
 def answerList():
-    answer_list = ['Paris', 'Berlin', 'Rome', 'Madrid', 'Washington D.C.', 'Ottawa', 'Mexico City', 'Brasilia', 'Buenos Aires', 'Canberra']
+    answer_list = ['Teachers Expectations, Current activity requirements, Social context and support, Available resources', 'Skill, Will, Self-Regulation', 'Examples can be found in Revised model PDF ', 'List found in Multiple Intelligence PDF', 'List found in Multiple Intelligence PDF.', 'Specific, Measurable, Attainable, Realistic, Timeable', 'Mexico City', 'Brasilia', 'Buenos Aires', 'Canberra']
     return answer_list
+
+main()
