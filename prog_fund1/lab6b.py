3fa8451e2ba9b3d786bcd167dad9f3270858bd3c
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-07 13:32:24 -0600

    Initial instructions for Lab6b and c

---
+++ prog_fund1/lab6b.py
@@ -0,0 +1,7 @@
+#Get name and average grade for a student
+#Grade can not be below 0 or above 100 - exception handling to ask for input again if invalid  
+#Repeat input for 12 students
+#Write all output to a file named grades.txt
+#Close the output file
+#Open the grades.txt, exception handling if file not found, asks for correct file name
+#Read the file and print the contents
---
+++ prog_fund1/lab6c.py
@@ -0,0 +1,6 @@
+#Write data to a file named coffeeInventory.txt with the following descriptions: Blonde Roast, Medium Roast, Flavored Roast, Dark Roast, Costa Rica Tarrazu
+#Write the following pounds to go with each of the above descriptions, 15, 21, 10, 12, 18
+#Read the file and display the data along with a sum of the total pounds of coffee
+#Append the following data to the file: Guatemala Antigua, 22 ; House Blend, 25 ; Decaf House Blend, 16
+#Ask if owner wants to modify the file by removing data, if yes, ask for a description to remove and remove it and it's pounds if it exists, otherwise display "That item was not found in the file"
+#Ask if the owner wants to delete data from the file, if yes ask for a description to delete, if it exists delete the name and quantity and replace it by asking for a new name and quantiity. If it does not exist, display "That item was not found in the file"
