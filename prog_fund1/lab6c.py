6f986f20d14ae9a1bf7134e6aae136690cf88800
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-10 14:51:40 -0600

    Initial logic for file initial data and appending for lab6c

--- prog_fund1/lab6c.py
+++ prog_fund1/lab6c.py
@@ -4,3 +4,48 @@
 #Append the following data to the file: Guatemala Antigua, 22 ; House Blend, 25 ; Decaf House Blend, 16
 #Ask if owner wants to modify the file by removing data, if yes, ask for a description to remove and remove it and it's pounds if it exists, otherwise display "That item was not found in the file"
 #Ask if the owner wants to delete data from the file, if yes ask for a description to delete, if it exists delete the name and quantity and replace it by asking for a new name and quantiity. If it does not exist, display "That item was not found in the file"
+
+def main():
+    #Define file name
+    fileName = 'coffeeInventory.txt'
+    #Write inital data to file
+    startupData = 'Blonde Roast, 15\nMedium Roast, 21\nFlavored Roast, 10\nDark Roast, 12\nCosta Rica Tarrazu, 18'
+    writeToFile(startupData, fileName)
+    #Read the file and print the contents
+    readFile(fileName)
+    #Append data to file
+    appendData = 'Guatemala Antigua, 22\nHouse Blend, 25\nDecaf House Blend, 16'
+    writeToFile(appendData, fileName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def writeToFile(writeValue, fileName): 
+    myFile = open(fileName, 'a')
+    myFile.write(writeValue + '\n')
+    myFile.close()
+def readFile(fileName):
+    try:
+        myFile = open(fileName, 'r')    
+        print(myFile.read())
+        myFile.close()
+    except FileNotFoundError:
+        print('File not found')
+        fileName = input('Enter the correct filename: ')
+        readFile(fileName)
