fb24a433c4529dbe4fd31971e514153db10ce3ba
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-09 19:54:30 -0600

    Adding input functions and file not found correcting to lab6b

--- prog_fund1/lab6b.py
+++ prog_fund1/lab6b.py
@@ -1,8 +1,9 @@
 
 def main():
-    writeToText('Something new')
-    writeToText("I'm here to help you with your grades!")
-    readText()
+    fileName = 'grades.txt'
+    writeToText('Something new', fileName)
+    writeToText("I'm here to help you with your grades!", fileName)
+    readText(fileName)
 #Get name and average grade for a student
 
 #Grade can not be below 0 or above 100 - exception handling to ask for input again if invalid  
@@ -12,14 +13,30 @@ def main():
 #Open the grades.txt, exception handling if file not found, asks for correct file name
 #Read the file and print the contents
 
-def writeToText(writeValue): 
-    myFile = open('grades.txt', 'a')
+def getStudentName():
+    studentName = input('Enter the student name: ')
+    return studentName
+
+def getStudentGrade():
+    try:
+        studentGrade = float(input('Enter the student grade: '))
+    except ValueError:
+        print('Invalid input. Please enter a number')
+    if studentGrade < 0 or studentGrade > 100:
+        print('Grade must be between 0 and 100')
+        studentGrade = getStudentGrade()
+    else:
+        studentGrade = str(studentGrade) 
+        return studentGrade       
+
+def writeToText(writeValue, fileName): 
+    myFile = open(fileName, 'a')
     myFile.write(writeValue + '\n')
     myFile.close()  
 
-def readText():
+def readText(fileName):
     try:
-        myFile = open('grades.txt')
+        myFile = open(fileName, 'r')
         thisLine = myFile.readline()
         while thisLine != '':
             print(thisLine)
@@ -27,5 +44,7 @@ def readText():
         myFile.close()
     except FileNotFoundError:
         print('File not found')
+        fileName = input('Enter the correct filename: ')
+        readText(fileName)
 
 main()
