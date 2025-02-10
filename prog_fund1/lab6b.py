6fb1dd6b4760880e1898da365f9912c7d7b09497
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-09 20:09:48 -0600

    Completed initial logic of lab6b

--- prog_fund1/lab6b.py
+++ prog_fund1/lab6b.py
@@ -1,50 +1,44 @@
-
+# Write a program that reads student names and grades from the user and writes them to a file.
 def main():
+    #Define file name and number of students
     fileName = 'grades.txt'
-    writeToText('Something new', fileName)
-    writeToText("I'm here to help you with your grades!", fileName)
-    readText(fileName)
-#Get name and average grade for a student
-
-#Grade can not be below 0 or above 100 - exception handling to ask for input again if invalid  
-#Repeat input for 12 students
-#Write all output to a file named grades.txt
-#Close the output file
-#Open the grades.txt, exception handling if file not found, asks for correct file name
-#Read the file and print the contents
-
+    numberOfStudents = 12
+    #Loop to get student name and grade, writing to file after each set of input
+    for i in range(numberOfStudents):
+        studentName = getStudentName()
+        studentGrade = getStudentGrade()
+        writeValue = studentName + ' ' + str(studentGrade)
+        writeToFile(writeValue, fileName)
+    #Read the file and print the contents            
+    readFile(fileName)
+#Function to get and return student name
 def getStudentName():
     studentName = input('Enter the student name: ')
     return studentName
-
+#Function to get and return grade with exception handling for non numbers and numbers outside of 0-100
 def getStudentGrade():
     try:
         studentGrade = float(input('Enter the student grade: '))
     except ValueError:
         print('Invalid input. Please enter a number')
-    if studentGrade < 0 or studentGrade > 100:
+    while studentGrade < 0 or studentGrade > 100:
         print('Grade must be between 0 and 100')
-        studentGrade = getStudentGrade()
-    else:
-        studentGrade = str(studentGrade) 
-        return studentGrade       
-
-def writeToText(writeValue, fileName): 
+        studentGrade = getStudentGrade() 
+    return studentGrade       
+#Function to write to file
+def writeToFile(writeValue, fileName): 
     myFile = open(fileName, 'a')
     myFile.write(writeValue + '\n')
     myFile.close()  
-
-def readText(fileName):
+#Function to read file, print contents, exception handling if file not found, asks for correct file name
+def readFile(fileName):
     try:
-        myFile = open(fileName, 'r')
-        thisLine = myFile.readline()
-        while thisLine != '':
-            print(thisLine)
-            thisLine = myFile.readline()
+        myFile = open(fileName, 'r')    
+        print(myFile.read())
         myFile.close()
     except FileNotFoundError:
         print('File not found')
         fileName = input('Enter the correct filename: ')
-        readText(fileName)
+        readFile(fileName)
 
 main()
