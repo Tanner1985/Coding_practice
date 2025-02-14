4336abf2c4814a5fb66b20e196e3735789797938
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-14 10:06:03 -0600

    Requirements for lab7

---
+++ prog_fund1/lab7a.py
@@ -0,0 +1,13 @@
+#Requirements:
+# 1. Create a table using a two-dimensional list that stores a Fahrenheit temperature and the equivalent Celsius temperature. Use the following range of Fahrenheit temperatures:
+# -10 through 100 in increments of 10. Thus the temperatures will be:
+# -10, 0, 10, 20, 30, 40, 50…and so on. (LO 1, 2, 3)
+# 2. Display the contents of the list. (LO 2)
+# 3. The program must calculate the values for the list (do not hard code the numbers)
+# 4. Similarly create tables/lists for:
+# a. Miles to kilometers
+# b. Gallons to liters
+# c. Pounds to kilograms
+# d. Inches to centimeters
+# Starting at 0 through 100 in increments of 10
+# 5. Display the contents of each list. 
