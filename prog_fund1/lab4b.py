7ce79befa28ff43944f96758b80c833cdd2d109a
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-17 10:02:21 -0600

    Extra inverted Triangles

--- prog_fund1/lab4b.py
+++ prog_fund1/lab4b.py
@@ -4,3 +4,15 @@ for r in range(rows):
     for s in range(rows - r):
         print('*', end='')
     print('')
+
+#Another way using multiplication, simpler
+for r in range (rows):
+    print('*' * (rows-r))
+
+#Tried with a while loop, works. 
+for r in range (rows):
+    s=0
+    while s < (rows - r):
+        print('*', end = '')
+        s = s + 1
+    print()
