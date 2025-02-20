26e4ebadfcfc2e44c0e3075b51156a8c37410827
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-20 06:36:41 -0600

    Small tweek to lab4c to make end of loop clear

--- prog_fund1/lab4c.py
+++ prog_fund1/lab4c.py
@@ -18,10 +18,12 @@ while grade != -1:
         print('You got a C. A bit more studying may be a good idea')
     elif grade >= 60:
         print('You got a D. You can do better, study a bit more')
+    elif grade == -1:
+        print('Done entering grades')
     else:
         print('You got an F! Did you study at all?')
 #Check if only 1 loop happened to very a valid grade was entered, then respond accordingly with adverage if real grades were entered
-if(numberGrades - 1) == 0:
+if(numberGrades) == 1:
     print ('No Grades entered')
 else:
     print(f'The total number of grades was {numberGrades - 1} and the class adverage was {int((totalGrade + 1)/(numberGrades - 1))}')
