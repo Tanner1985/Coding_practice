6f230acf0718bdc26412c06c3069305d5e38bc3d
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-20 06:39:19 -0600

    Correcting double condition check

--- prog_fund1/lab4c.py
+++ prog_fund1/lab4c.py
@@ -9,7 +9,7 @@ while grade != -1:
     totalGrade = totalGrade + grade
     #Check grade and give feedback on their letter value
     if grade == -1:
-        print('Escape value entered')
+        print('Done entering grades')
     elif grade >= 90:
         print('You got an A! Great job')
     elif grade >= 80:
@@ -18,8 +18,6 @@ while grade != -1:
         print('You got a C. A bit more studying may be a good idea')
     elif grade >= 60:
         print('You got a D. You can do better, study a bit more')
-    elif grade == -1:
-        print('Done entering grades')
     else:
         print('You got an F! Did you study at all?')
 #Check if only 1 loop happened to very a valid grade was entered, then respond accordingly with adverage if real grades were entered
