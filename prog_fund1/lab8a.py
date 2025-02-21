8f25c232d3f851271afd9b22d5aa215fcca5d06f
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-21 13:07:12 -0600

    Adding name and email entering for lab8 logic with email validation

--- prog_fund1/lab8a.py
+++ prog_fund1/lab8a.py
@@ -6,46 +6,51 @@
 
 #3. When you display the conversion output to the user, you must include the user’s
 #name in the output.
-
+# #Get name and email
+userName = input('Please enter your name: ')
+email = input('Please enter your email:  ')
+#Verify @ is in the email
+while '@' not in email:
+    email = input('No @ found. Please enter a valid email address: ')
 # Get each value for conversion and make sure they are valid using a loop to allow for up to 3 attempts at the input
-miles_converter = float(input('William, please input the miles you wish to convert to kilometers '))
+miles_converter = float(input(f'{userName}, please input the miles you wish to convert to kilometers '))
 bad_counter = 0
 while miles_converter < 0 and bad_counter < 2:
     print('Negative number for miles detected. No running backwards please')
-    miles_converter = float(input('William, please input the miles you wish to convert to kilometers '))
+    miles_converter = float(input(f'{userName}, please input the miles you wish to convert to kilometers '))
     bad_counter = bad_counter + 1
 if bad_counter == 2:
     print('Ran backwards too many times')
 else:
-    fahren_converter = float(input('William, please input the degrees in fahrenheit you wish to convert to Celsius '))
+    fahren_converter = float(input(f'{userName}, please input the degrees in fahrenheit you wish to convert to Celsius '))
     bad_counter = 0
     while fahren_converter > 1000 and bad_counter < 2:
         print('To large a number for temperature conversion. No fires please')
-        fahren_converter = float(input('William, please input the degrees in fahrenheit you wish to convert to Celsius '))
+        fahren_converter = float(input(f'{userName}, please input the degrees in fahrenheit you wish to convert to Celsius '))
         bad_counter = bad_counter + 1 
     if bad_counter == 2:
         print('Too many fires. You may be on the sun')
     else: 
-        gallons_converter = float(input('William, please input the gallons you wish to convert to liters '))
+        gallons_converter = float(input(f'{userName}, please input the gallons you wish to convert to liters '))
         while gallons_converter < 0 and bad_counter < 2:
             print ('Negative number for gallons detected. Keep hydrated please')
-            gallons_converter = float(input('William, please input the gallons you wish to convert to liters '))
+            gallons_converter = float(input(f'{userName}, please input the gallons you wish to convert to liters '))
             bad_counter = bad_counter + 1 
         if bad_counter == 2:
             print('Program dehydrated from too many negatives')    
         else:
-            pounds_converter = float(input('William, please input the pounds you wish to convert to kilograms '))
+            pounds_converter = float(input(f'{userName}, please input the pounds you wish to convert to kilograms '))
             while pounds_converter < 0 and bad_counter < 2:
                 print ('Negative number for pounds detected. Please patent your antigravity technology')
-                pounds_converter = float(input('William, please input the pounds you wish to convert to kilograms '))
+                pounds_converter = float(input(f'{userName}, please input the pounds you wish to convert to kilograms '))
                 bad_counter = bad_counter + 1
             if bad_counter == 2:   
                 print('Too much antigravity, now on the moon')    
             else:    
-                inches_converter = float(input('William, please input the inches you wish to convert to centimeters '))
+                inches_converter = float(input(f'{userName}, please input the inches you wish to convert to centimeters '))
                 while inches_converter < 0 and bad_counter < 2:
                     print ('Negative number for inches detected. No playing with shrink-rays')
-                    inches_converter = float(input('William, please input the inches you wish to convert to centimeters '))
+                    inches_converter = float(input(f'{userName}, please input the inches you wish to convert to centimeters '))
                     bad_counter = bad_counter + 1
                 if bad_counter == 2:    
                     print('Too much shrinking, can no longer see')    
@@ -57,10 +62,9 @@ else:
                     poundsToKl = pounds_converter * .45
                     inchesToCm = inches_converter * 2.54
 
-
 #Return all the converted values with bad puns
-                    print (f'William, {miles_converter} miles is {milesToKm:.2f} kilometers. Quite a long shot huh?')
-                    print (f'William, {fahren_converter} degrees fahrenheit is {fahrenheitToC:.2f} degrees celsius. That is so cool!')
-                    print (f'William, {gallons_converter} gallons comes out to {gallonsToL:.2f} liters. Should I turn down the volume?')
-                    print (f'William, {pounds_converter} pounds turns into {poundsToKl:.2f} kilograms. That was some heavy math!')
-                    print (f'William, {inches_converter} inches is {inchesToCm:.2f} centimeters. I have no more puns, but that was fun!')
+                    print (f'{userName}, {miles_converter} miles is {milesToKm:.2f} kilometers. Quite a long shot huh?')
+                    print (f'{userName}, {fahren_converter} degrees fahrenheit is {fahrenheitToC:.2f} degrees celsius. That is so cool!')
+                    print (f'{userName}, {gallons_converter} gallons comes out to {gallonsToL:.2f} liters. Should I turn down the volume?')
+                    print (f'{userName}, {pounds_converter} pounds turns into {poundsToKl:.2f} kilograms. That was some heavy math!')
+                    print (f'{userName}, {inches_converter} inches is {inchesToCm:.2f} centimeters. I have no more puns, but that was fun!')
