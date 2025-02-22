7f2e3d5b5589d820f41e8307a74fe1ce34d3b058
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-21 20:10:09 -0600

    Update student class comments

--- prog_fund1/student.py
+++ prog_fund1/student.py
@@ -1,12 +1,12 @@
 class Student:
-    
+    #Define date for the class
     def __init__(self, name, ID, GPA, expectGrade,fullOrPart):
         self.__name = name
         self.__ID = ID
         self.__GPA = GPA
         self.__expectGrade = expectGrade
         self.__fullOrPart = fullOrPart
-
+    #define functions to make changes to the data in the class based on user input
     def setName(self):
         self.__name = input('Please enter the students name: ')
     def setID(self):
@@ -17,6 +17,7 @@ class Student:
         self.__expectGrade = input('Please enter the students expected grade: ')
     def setFullorPart(self):
         self.__fullOrPart = input('Please enter if the student is full or part time: ')
+    #define functions to fetch specific data from class
     def getName(self):
         return self.__name
     def getID(self):
