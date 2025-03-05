c70409c346c99d8cd2ca76c8a413df615a2aff7d
Tanner <tanner.hoekstra1985@gmail.com>
2025-03-05 11:08:53 -0600

    Initial logic for playing with files

---
+++ studyguides/files.py
@@ -0,0 +1,34 @@
+#Set of file manipulation tests to mess around with
+
+def main():
+    yourFile = input('Please give a file name to use: ')
+    firstLine = input('Type something you would like to put in a file: ')
+    writeLineToFile(yourFile ,firstLine)
+    readFile(yourFile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Writes a single line to a file
+def writeLineToFile(fileName, line):
+    myFile = open (fileName, 'a')
+    myFile.write(line +'\n')
+    myFile.close
+#Function to read and print entire file given
+def readFile(filename):
+    with open (filename, 'r') as readingFile:
+        print(readingFile.read())
+
+main()
