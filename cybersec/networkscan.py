cf84cb729245a98c9c01197feda431faf66903c4
Tanner1985 <tanner.hoekstra1985@gmail.com>
2025-09-18 16:01:11 -0400

    Added multiprocessing to network scan to speed up the process

--- cybersec/networkscan.py
+++ cybersec/networkscan.py
@@ -1,6 +1,9 @@
 import scapy.all as scapy
 import machine
 import ipaddress
+import multiprocessing
+import time
+MACHINELIST = []
 #Basic network scanner, returns info to the console
 def netScan():
     network = input('Please Enter the Network to scan: ')
@@ -17,24 +20,38 @@ def netScan():
         print(element[1].psrc + "      " + element[1].hwsrc)
 #Scanner that returns a list of objects with IP and MAC info from the scan
 def netScanSave():
-    machineList = []
+    processes = []
+    startTime = time.time()
     network = input('Please Enter the Network to scan: ')
-    networkToScan = list(ipaddress.ip_network(network).subnets(new_prefix=24))[0]
+    network = ipaddress.ip_network(network, strict=False)
+    networkToScan = list(network.subnets(new_prefix=24))
     startPort = int(input('Please enter the start of the port range to scan for each machine: '))
     endPort = int(input('Please enter the end of the port range to scan for each machine: '))
     for element in networkToScan:
-        request = scapy.ARP()
-        request.pdst = network
-        broadcast = scapy.Ether()
-        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+        numProcesses = 16
+        with multiprocessing.Pool(processes=numProcesses) as pool:
+            p = pool.map(autoNetScan, str(element),)
+            processes.append(p)
+    for process in processes:
+        process.join()
+    endTime = time.time()
+    print(f'Scan completed in {endTime - startTime} seconds')
+    for machine in MACHINELIST:
+        machine.scanSavePorts(startPort, endPort)
+    return MACHINELIST
+            ##machine.scanSavePorts(startPort, endPort)
+def autoNetScan(network):
+    request = scapy.ARP()
+    request.pdst = network
+    broadcast = scapy.Ether()
+    broadcast.dst = 'ff:ff:ff:ff:ff:ff'
 
-        request_broadcast = broadcast / request
-        clients = scapy.srp(request_broadcast, timeout = 1)[0]
-        for element in clients:
+    request_broadcast = broadcast / request
+    clients = scapy.srp(request_broadcast, timeout = 1)[0]
+    for element in clients:
             machine = createNewMachine(element[1].psrc, element[1].hwsrc)
-            machineList.append(machine)
-            machine.scanSavePorts(startPort, endPort)
-    return machineList
+            MACHINELIST.append(machine)
+
 def createNewMachine(IP, MAC):
     return machine.Machine(IP, MAC)
 
