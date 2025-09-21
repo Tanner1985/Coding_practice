5a7e8d806aba51c9eb0459457c04dace7a468963
Tanner1985 <tanner.hoekstra1985@gmail.com>
2025-09-21 14:16:21 -0400

    Removing custom port scanner from machine class

--- cybersec/machine.py
+++ cybersec/machine.py
@@ -18,14 +18,6 @@ class Machine:
         return self.__MAC
     def getOpenPorts(self):
         return self.__openPorts
-    def scanPorts(self, startPort, endPort):
-        for port in range(startPort, endPort + 1):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(0.5)
-            result = sock.connect_ex((self.__IP, port))
-            if result == 0:
-                self.setOpenPorts(port)
-            sock.close()
     def scanUnsecurePorts(self):
         unsecurePorts = [20,21,22,23,25,53,67,68,69,80,110,111,135,137,138,139,143,161,162,389,443,445,512,513,514,1099,2049,2121,3306,3389,5900,6000,6667,8080]
         for port in unsecurePorts:
