88238a3d45762428cfb5805fbaefcb48cf39efa5
Tanner1985 <tanner.hoekstra1985@gmail.com>
2025-09-23 10:53:13 -0400

    Code to split network into subnets to solve issues with large networks

--- cybersec/networkscan.py
+++ cybersec/networkscan.py
@@ -3,21 +3,21 @@ import machine
 import ipaddress
 #Basic network scanner, returns info to the console
 def netScan(network):
-    request = scapy.ARP()
+    clients = []
+    for subnet in getSubnets(network):
+        request = scapy.ARP()
+        request.pdst = subnet.network_address.exploded + '/16'
+        broadcast = scapy.Ether()
 
-    request.pdst = network
-    broadcast = scapy.Ether()
+        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
 
-    broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-
-    request_broadcast = broadcast / request
-    clients = scapy.srp(request_broadcast, timeout = 1)[0]
+        request_broadcast = broadcast / request
+        clients.append(scapy.srp(request_broadcast, timeout = 1)[0])
     for element in clients:
         print(element[1].psrc + "      " + element[1].hwsrc)
     return clients
 #Scanner that returns a list of objects with IP and MAC info from the scan
 
-def getSubnet(ip, cidr):
-    network = ipaddress.ip_network(f"{ip}/{cidr}", strict=False)
-    return str(network)
+def getSubnets(network):
+    return list(ipaddress.ip_network(network).subnets(new_prefix=16))
 
