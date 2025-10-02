74ef7f178a3c97309ac338845c94f9492d6fbf13
Tanner1985 <tanner.swift1985@gmail.com>
2025-10-02 12:38:49 -0500

    Quick trace route program using graphviz to display

---
+++ cybersec/trace.py
@@ -0,0 +1,16 @@
+#This is a complex trace route using scapy and saving to a graph
+import scapy.all as scapy
+import graphviz
+
+def main():
+    traceList = []
+    moreTrace = 'Y'
+    while moreTrace != 'N':
+        target = input('Please enter the site or IP to trace to: ')
+        traceList.append(target)
+        moreTrace = input('Would you like to add more sites to trace? Y or N: ')
+        moreTrace.upper()
+    res, unans = scapy.traceroute(traceList, dport=[80,443],maxttl=20,retry=-2)
+    res.graph()
+
+main()
