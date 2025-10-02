1d80c684a0229326458fa929d696fb8d47073fab
Tanner1985 <tanner.swift1985@gmail.com>
2025-10-02 11:01:56 -0500

    adding different ways to scan networks

--- cybersec/main_security.py
+++ cybersec/main_security.py
@@ -7,9 +7,9 @@ import subprocess
 import machine
 #This is the main module that elevates permissions and orginizes the flow of information
 def main():
-    if os.geteuid() !=0:
-        print('Must be a sudo user, switching')
-        runSudo()
+    ##if os.geteuid() !=0:
+        ##print('Must be a sudo user, switching')
+        ##runSudo()
     choice = getChoice()
     while choice !=0:
         if choice == 1:
@@ -24,7 +24,7 @@ def main():
             choice = getChoice()
         elif choice == 4:
             network = input('Please Enter the Network to scan: ')
-            networkList = networkscan.netScan(network)
+            networkList = networkscan.netARPScan(network)
             print(networkList)
             for client in networkList:
                 for item in client:
@@ -34,6 +34,27 @@ def main():
                     print(f'Scanning {ip} for unsecure ports')
                     newMachine.scanUnsecurePorts()
                     print(f'Open ports for {ip} are: {newMachine.getOpenPorts()}')
+            choice = getChoice()
+        elif choice == 5:
+            network = input('Please Enter the Network to scan: ')
+            networkList = networkscan.scanICMP(network)
+            choice = getChoice()
+        elif choice == 6:
+            network = input('Please Enter the Network to scan: ')
+            networkList = networkscan.scanTCP(network)
+            choice = getChoice()
+        elif choice == 7:
+            network = input('Please Enter the Network to scan: ')
+            networkList = networkscan.pingARP(network)
+            choice = getChoice()
+        elif choice == 8:
+            network = input('Please Enter the Network to scan: ')
+            switch = input('Please enter MAC of target network device: ')
+            networkList = networkscan.netARPCustomMac(network, switch)
+            choice = getChoice()
+        elif choice == 9:
+            network = input('Please Enter the Network to scan: ')
+            networkList = networkscan.UDPping(network)
             choice = getChoice()
         else:
             print('Invalid option given')
@@ -48,15 +69,23 @@ def getChoice():
     print('Please pick an option')
     print('---------------------------')
     print('0: Exit')
-    print('1: Network Scanner, scan an IP range for devices')
+    print('1: Network Scanner, scan an IP range for devices using ARP')
     print('2: Port Scanner, scan an IP and port range for open ports')
     print('3: API request, pull from an API and save JSON to text')
-    print('4: Scan a Network and then each found machines for specific port ranges ')
+    print('4: Scan a Network via ARP and then each found machines for specific port ranges ')
+    print('5: Scan a network using ICMP')
+    print('6: Scan a network using TCP')
+    print('7: Scan a network using arping')
+    print('8: ARP scan with specific MAC input')
+    print('9: Scan using UDP')
     print('---------------------------')
     choice = int(input('Type your number of choice here: '))
     return choice
 
 def createNewMachine(IP, MAC):
-    return machine.Machine(IP, MAC)
+    thisMachine = machine.Machine()
+    thisMachine.setIP = IP
+    thisMachine.setMAC = MAC
+    return thisMachine
 
 main()
--- cybersec/networkscan.py
+++ cybersec/networkscan.py
@@ -1,8 +1,8 @@
 import scapy.all as scapy
 import machine
 import ipaddress
-#Basic network scanner, returns info to the console
-def netScan(network):
+#Scanner that returns a list of objects with IP and MAC info from the scan using ARP
+def netARPScan(network):
     clients = []
     for subnet in getSubnets(network):
         request = scapy.ARP()
@@ -18,7 +18,63 @@ def netScan(network):
             for item in element:
                 print(item[1].psrc + "      " + item[1].hwsrc)
     return clients
-#Scanner that returns a list of objects with IP and MAC info from the scan
+def scanICMP(network):
+    clients = []
+    for subnet in getSubnets(network):
+        request = scapy.ICMP()
+        request.pdst = subnet.network_address.exploded + '/16'
+        broadcast = scapy.Ether()
+
+        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+
+        requestBroadcast = broadcast / request
+        clients.append(scapy.srp(requestBroadcast, timeout = 1600)[0])
+        for element in clients:
+            print (element)
+def scanTCP(network):
+    clients = []
+    for subnet in getSubnets(network):
+        request = scapy.TCP(sport=666,flags="FPU")
+        request.pdst = subnet.network_address.exploded + '/16'
+        broadcast = scapy.Ether()
+
+        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+        requestBroadcast = broadcast / request
+        clients.append(scapy.srp(requestBroadcast, timeout = 1600)[0])
+        for element in clients:
+            print (element)
+def pingARP(network):
+    scapy.arping(network)
+
+def UDPping(network):
+        clients = []    
+        request = scapy.UDP(dport=0)
+        request.pdst = network
+        broadcast = scapy.Ether()
+
+        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+        requestBroadcast = broadcast / request
+        clients.append(scapy.srp(requestBroadcast, timeout = 1600)[0])
+        for element in clients:
+            print (element)
+
+
+def netARPCustomMac(network, switch):
+    clients = []
+    for subnet in getSubnets(network):
+        request = scapy.ARP()
+        request.pdst = subnet.network_address.exploded + '/16'
+        broadcast = scapy.Ether()
+
+        broadcast.dst = switch
+
+        request_broadcast = broadcast / request
+        clients.append(scapy.srp(request_broadcast, timeout = 1)[0])
+        for element in clients:
+            print (element)
+            for item in element:
+                print(item[1].psrc + "      " + item[1].hwsrc)
+    return clients
 
 def getSubnets(network):
     return list(ipaddress.ip_network(network).subnets(new_prefix=16))
